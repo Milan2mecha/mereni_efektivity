18750efa6f4dce067cb52e11dc799348b7052761
Milan 2 <kopper@spstrutnov.cz>
2024-09-22 15:46:14 +0200

    Vytvoření  ADS121X

--- libraries/ADS111X.h
+++ libraries/ADS111X.h
@@ -1,5 +1,4 @@
 
-
 //#ifndef ADS111X_addr
 #define ADS111X_addr    0b10000000
 #define AIN0 0
@@ -10,8 +9,13 @@
 #define calibration 5
 
 void ADS111X_init(void);
-uint16_t ADS111X_read(void);
+uint8_t ADS111X_VREF(uint8_t);
 uint8_t ADS111X_RREG(uint8_t);
 uint8_t ADS111X_MUX(uint8_t, uint8_t);
+uint8_t ADS111X_CM(uint8_t);
 void ADS111X_cal(void);
 uint16_t ADS111X_DR(uint16_t);
+void ADS111X_POWERDOWN(void);
+
+int16_t ADS111X_measure(void);
+int16_t ADS111X_measure_sg(void);
---
+++ libraries/ADS121X .h
@@ -0,0 +1,22 @@
+
+
+//#ifndef ADS121X_addr
+#define ADS121X_addr    0b10000000
+#define AIN0 0
+#define AIN1 1
+#define AIN2 2 
+#define AIN3 3
+#define AGND 4
+#define calibration 5
+
+void ADS121X_init(void);
+uint8_t ADS121X_VREF(uint8_t);
+uint8_t ADS121X_RREG(uint8_t);
+uint8_t ADS121X_MUX(uint8_t, uint8_t);
+uint8_t ADS121X_CM(uint8_t);
+void ADS121X_cal(void);
+uint16_t ADS121X_DR(uint16_t);
+void ADS121X_POWERDOWN(void);
+
+int16_t ADS121X_measure(void);
+int16_t ADS121X_measure_sg(void);
